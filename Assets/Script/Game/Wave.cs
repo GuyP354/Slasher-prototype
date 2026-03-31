63618017d58ab179773bad4d5035cb122bdd8f25
GuyP354 <guyporat2004@gmail.com>
2026-03-31 16:35:14 +0100

    Waves

--- Assets/Script/Game/Wave.cs
+++ Assets/Script/Game/Wave.cs
@@ -1,113 +1,109 @@
 using UnityEngine;
 using System.Collections.Generic;
-
 public class Wave : MonoBehaviour
 {
     public List<WaveEvent> events = new();
-    private bool isPlaying = false;
-
+    private bool isPlaying;
+    private bool waitingForNextEvent;
     public void StartWave()
     {
         isPlaying = true;
-
-        if (events.Count != 0)
-            events[0].StartEvent();
-        else
+        waitingForNextEvent = false;
+        if (events.Count == 0)
         {
             LevelManager.Instance.EndWave();
+            return;
         }
+        waitingForNextEvent = true;
+        Debug.Log("Press E to start wave");
     }
-
     private void Update()
     {
-        if (!isPlaying)
+        if (!isPlaying) return;
+        // If we finished an event, wait for E to start the next one
+        if (waitingForNextEvent)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                waitingForNextEvent = false;
+                if (events.Count == 0)
+                    LevelManager.Instance.EndWave();
+                else
+                    events[0].StartEvent();
+            }
             return;
-
+        }
+        // Run current event
+        if (events.Count == 0)
+        {
+            LevelManager.Instance.EndWave();
+            return;
+        }
         if (!events[0].RunEvent())
         {
             Debug.Log("End Event");
             events.RemoveAt(0);
-
             if (events.Count == 0)
+            {
                 LevelManager.Instance.EndWave();
-            
+            }
             else
-                events[0].StartEvent();
+            {
+                // Don't auto-start next event; wait for E
+                waitingForNextEvent = true;
+                Debug.Log("Press E to start next event");
+            }
         }
     }
-
     [System.Serializable]
     public class WaveEvent
     {
         public float duration = 15.0f;
-        public List<SpawnInfo> spawnInfos;
-
+        public List<SpawnInfo> spawnInfos = new();
         private float startTime;
-
         public void StartEvent()
         {
             startTime = Time.time;
             foreach (var info in spawnInfos)
-            {
                 info.Initialize();
-            }
         }
-
         public bool RunEvent()
         {
             if (duration == 0.0f && spawnInfos.Count == 0)
                 return false;
-            else if (Time.time - startTime > duration && duration != 0.0f)
+            if (duration != 0.0f && Time.time - startTime > duration)
                 return false;
-
-
             for (int i = 0; i < spawnInfos.Count; i++)
             {
                 spawnInfos[i].ReadyToSpawn();
-
-                if (spawnInfos[i].amount == 0)
+                if (spawnInfos[i].amount <= 0)
                 {
                     spawnInfos.RemoveAt(i);
                     i--;
                 }
             }
-
             return true;
         }
-
         [System.Serializable]
         public class SpawnInfo
-
         {
             public int spawnPointIndex = 0;
             public int spawnPrefabIndex = 0;
             public int amount = 10;
             public float interval = 1.0f;
-
             private float lastTime;
-
             public void Initialize()
             {
                 lastTime = Time.time;
             }
-
             public void ReadyToSpawn()
             {
+                if (amount <= 0) return;
                 if (Time.time - lastTime >= interval)
                 {
                     SpawnManager.Instance.Spawn(spawnPrefabIndex, spawnPointIndex);
                     lastTime = Time.time;
                     amount--;
-                }
-
-                void ReadyToSpawn()
-                {
-                    if (Time.time - lastTime >= interval)
-                    {
-                        SpawnManager.Instance.Spawn(spawnPrefabIndex, spawnPointIndex);
-                        amount--;
-                        lastTime = Time.time;
-                    }
                 }
             }
         }
