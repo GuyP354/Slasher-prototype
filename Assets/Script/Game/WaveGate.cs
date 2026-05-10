6f6ff940df73bc6e254be32b51d238b93b7686c1
GuyP354 <guyporat2004@gmail.com>
2026-05-10 10:59:17 +0100

    Bug fixesz

--- Assets/Script/Game/WaveGate.cs
+++ Assets/Script/Game/WaveGate.cs
@@ -194,12 +194,6 @@ public class WaveGate : MonoBehaviour
 
     private void HandleWaveElementEnded(bool waveFullyComplete)
     {
-        int taggedEnemies = GameObject.FindGameObjectsWithTag("Enemy").Length;
-        int trackedEnemies = SpawnManager.Instance != null ? SpawnManager.Instance.GetEnemiesLeft() : 0;
-
-        if (Mathf.Max(taggedEnemies, trackedEnemies) == 0)
-            Open();
-        else
-            Close();
+        Open();
     }
 }
